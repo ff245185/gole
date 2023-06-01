3a430ffaa14a4595db00480801b867390ac247ef
unknown <ff245185@gmail.com>
2023-06-01 12:14:12 +0600

    fooyter added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './page/Home/Footer/Navebar/Home'
+import Navebar from './page/Home/Footer/Navebar/Navebar';
+import Footer from './page/Home/Footer/Navebar/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1 className="text-3xl font-bold ">
-      you are the  gretest nither fucker <i>in the wold </i>
-    </h1>
-      </header>
+       <Navebar></Navebar>
+       <Home></Home>
+       <Footer></Footer>
     </div>
   );
 }
